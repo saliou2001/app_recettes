e0c52a46fdffe9841c87b26c8bd25fca805b9ade
Fatou SOW <86850873+fatousow1997@users.noreply.github.com>
2024-02-20 12:05:49 +0100

    codage du menu

--- src/main/java/AppTest/Main.java
+++ src/main/java/AppTest/Main.java
@@ -8,12 +8,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        String reset = "\u001B[0m";
+        String cyanBold = "\u001B[1;36m";
+        String yellowBold = "\u001B[1;33m";
+        String purpleBold = "\u001B[1;35m";
+        String greenBold = "\u001B[1;32m";
+        String star = "★";
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Veuillez sélectionner le mode d'affichage :");
-        System.out.println("1. Menu Textuel");
-        System.out.println("2. Menu Graphique");
-        System.out.print("Votre choix : ");
+        // Texte "Bienvenue dans l'application"
+        String bienvenueTexte = "Bienvenue dans l'application";
+        // Calcul de la longueur de la ligne pour centrer le texte
+        int longueurLigne = 60;
+        int espacesAvant = (longueurLigne - bienvenueTexte.length()) / 2;
+        int espacesApres = longueurLigne - bienvenueTexte.length() - espacesAvant;
+
+        // Affichage du texte centré avec des étoiles décoratives et des couleurs
+        System.out.println(cyanBold + "*".repeat(longueurLigne) + reset);
+        System.out.println(cyanBold + "*".repeat(espacesAvant) + yellowBold + bienvenueTexte + reset +
+                cyanBold + "*".repeat(espacesApres) + reset);
+        System.out.println(cyanBold + "*".repeat(longueurLigne) + reset);
+
+        // Affichage du menu avec des couleurs pour chaque partie du texte
+        System.out.println(yellowBold + greenBold + star + " " + cyanBold + "1. " + reset + yellowBold + "Menu Textuel" + reset +
+                "\t\t" + yellowBold + greenBold + star + " " + cyanBold + "2. " + reset + yellowBold + "Menu Graphique" + reset +
+                "\t\t" + purpleBold + star + " " + cyanBold + "0. " + reset + purpleBold + "Quitter" + reset);
+        System.out.print(yellowBold + "Votre choix : " + reset);
 
         int choice = scanner.nextInt();
 
